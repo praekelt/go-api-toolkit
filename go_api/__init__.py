29352c081f5738870ec178554b14ee6050e4edf5
justinvdm <justinvdm.0.1.0@gmail.com>
2014-07-17 13:47:54 +0200

    Version 0.1.2

--- go_api/__init__.py
+++ go_api/__init__.py
@@ -1,5 +1,5 @@
 """A toolkit for writing Vumi Go APIs"""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = []
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="go_api",
-    version="0.1.1",
+    version="0.1.2",
     url='http://github.com/praekelt/go-api-toolkit',
     license='BSD',
     description="A toolkit for building Vumi Go APIs",
