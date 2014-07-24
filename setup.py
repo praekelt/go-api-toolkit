01169d345f7974b134ab0e20c0f721b4f7e0bf41
Jeremy Thurgood <jeremy@praekelt.com>
2014-07-24 11:31:14 +0200

    Set version for release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="go_api",
-    version="0.1.5a",
+    version="0.1.5",
     url='http://github.com/praekelt/go-api-toolkit',
     license='BSD',
     description="A toolkit for building Vumi Go APIs",
