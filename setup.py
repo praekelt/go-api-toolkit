94a1fab49c2e21cf164e0972fca4a3a30ebba340
Rudi Giesler <rudi@praekelt.com>
2014-08-27 11:52:35 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="go_api",
-    version="0.1.10a",
+    version="0.2.1a",
     url='http://github.com/praekelt/go-api-toolkit',
     license='BSD',
     description="A toolkit for building Vumi Go APIs",
