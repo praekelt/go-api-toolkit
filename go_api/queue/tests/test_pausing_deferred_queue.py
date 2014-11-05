061bc5b16f5cfdfe42793a4adf7ae019e357d19e
Rudi Giesler <rudi@praekelt.com>
2014-11-05 16:01:12 +0200

    Moved test setUp into individual tests for PausingDeferredQueue tests

--- go_api/queue/tests/test_pausing_deferred_queue.py
+++ go_api/queue/tests/test_pausing_deferred_queue.py
@@ -26,32 +26,31 @@ class ImmediateFailureMixin(object):
 class TestPausingDeferredQueue(
         unittest.SynchronousTestCase, ImmediateFailureMixin):
 
-    def setUp(self):
-        self.size = 3
-        self.backlog = 2
-        self.q = PausingDeferredQueue(size=self.size, backlog=self.backlog)
-
     def test_empty_queue_underflow(self):
         """
         When the total amount of deferred gets is exceeded, a L{QueueUnderflow}
         error is raised.
         """
-        for i in range(self.backlog):
-            self.q.get()
-        self.assertRaises(defer.QueueUnderflow, self.q.get)
+        backlog = 2
+        q = PausingDeferredQueue(size=3, backlog=2)
+        for i in range(backlog):
+            q.get()
+        self.assertRaises(defer.QueueUnderflow, q.get)
 
     def test_backlog_queue(self):
         """
         If there is a backlog of gets for a queue, they are fulfilled when
         values are placed into the queue.
         """
+        backlog = 2
+        q = PausingDeferredQueue(size=3, backlog=backlog)
         gotten = []
         # Create backlog
-        for i in range(self.backlog):
-            self.q.get().addCallback(gotten.append)
+        for i in range(backlog):
+            q.get().addCallback(gotten.append)
         # Fill queue to satisfy backlog
-        for i in range(self.backlog):
-            d = self.q.put(i)
+        for i in range(backlog):
+            d = q.put(i)
             self.assertEqual(self.successResultOf(d), None)
             self.assertEqual(gotten, list(range(i + 1)))
 
@@ -61,23 +60,25 @@ class TestPausingDeferredQueue(
         object to the queue, the returned defer will only fire if an object is
         removed from the queue.
         """
-        for i in range(self.size - 1):
-            d = self.q.put(i)
+        size = 3
+        q = PausingDeferredQueue(size=size, backlog=2)
+        for i in range(size - 1):
+            d = q.put(i)
             self.assertEqual(self.successResultOf(d), None)
 
         # This next put fills the queue, so the deferred we return will only
         # get its result when the queue shrinks.
-        put_d = self.q.put(self.size)
+        put_d = q.put(size)
         self.assertNoResult(put_d)
 
         # When we pull something out of the queue, put_d fires and we're able
         # to put another thing into the queue.
         gotten = []
-        self.q.get().addCallback(gotten.append)
+        q.get().addCallback(gotten.append)
         self.assertEqual(gotten, [0])
         self.assertEqual(self.successResultOf(put_d), None)
 
-        put_d = self.q.put(self.size)
+        put_d = q.put(size)
         self.assertNoResult(put_d)
 
     def test_queue_overflow(self):
@@ -85,32 +86,34 @@ class TestPausingDeferredQueue(
         If you try to add more elements than size, a L{QueueOverflow} error
         will be thrown.
         """
-        for i in range(self.size):
-            self.q.put(i)
+        size = 3
+        q = PausingDeferredQueue(size=size, backlog=2)
+        for i in range(size):
+            q.put(i)
 
-        self.assertRaises(defer.QueueOverflow, self.q.put, None)
+        self.assertRaises(defer.QueueOverflow, q.put, None)
 
     def test_queue_no_limits(self):
         """
         You can put and get objects from the queue when there are no limits
         supplied.
         """
-        self.q = PausingDeferredQueue()
+        q = PausingDeferredQueue()
         gotten = []
-        for i in range(self.size):
-            self.q.get().addCallback(gotten.append)
-        for i in range(self.size):
-            d = self.q.put(i)
+        for i in range(3):
+            q.get().addCallback(gotten.append)
+        for i in range(3):
+            d = q.put(i)
             self.assertEqual(self.successResultOf(d), None)
-        self.assertEqual(gotten, list(range(self.size)))
+        self.assertEqual(gotten, list(range(3)))
 
     def test_zero_size_overflow(self):
         """
         A L{QueueOverflow} error is raised when there is a put request on a
         queue of size 0
         """
-        self.q = PausingDeferredQueue(size=0)
-        self.assertRaises(defer.QueueOverflow, self.q.put, None)
+        q = PausingDeferredQueue(size=0)
+        self.assertRaises(defer.QueueOverflow, q.put, None)
 
     def test_zero_backlog_underflow(self):
         """
